5f41c716f18314093fe2843aceb4590fd478a765
Rifushigi <tope.dansa@gmail.com>
2025-04-14 06:36:03 +0100

    Update lecturer interface to include TODO for departmental course review

--- backend/src/types/lecturer.ts
+++ backend/src/types/lecturer.ts
@@ -4,5 +4,6 @@ export interface TLecturer extends Document {
     _id: ObjectId;
     userId: ObjectId;
     staffId: string;
+    //TODO need to review this due to the fact that different lecuturers take courses between different departments
     department: string;
 }
