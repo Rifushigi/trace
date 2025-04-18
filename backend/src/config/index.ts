f67b5ec35b5100814c7789453d154f665df7308c
Rifushigi <tope.dansa@gmail.com>
2025-04-18 15:59:38 +0100

    Add configuration exports for constants, database, cloudinary, multer, CORS, session, and mailer

---
+++ backend/src/config/index.ts
@@ -0,0 +1,7 @@
+export * from './constants_config.js';
+export * from './db_config.js';
+export { default as cloudinary } from './cloudinary_config.js';
+export { default as upload } from './multer_config.js'
+export * from './cors_config.js';
+export * from './session_config.js';
+export * from './mailer_config.js';
