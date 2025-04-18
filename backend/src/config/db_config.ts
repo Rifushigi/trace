a6c08249ae5ef7ffcecdfb2b0d9145e675765a2e
Rifushigi <tope.dansa@gmail.com>
2025-04-18 15:59:30 +0100

    Add database configuration and initialization functions

---
+++ backend/src/config/db_config.ts
@@ -0,0 +1,22 @@
+import { connect } from 'mongoose';
+import { env, localDBUrl, prdDBUrl } from './constants_config.js';
+import { TDBConfig } from '../types/index.js';
+import { DatabaseError } from '../middlewares/index.js';
+
+export const getDBUrl = (): Pick<TDBConfig, 'url'> => {
+    if (env != 'production') {
+        return { url: localDBUrl! };
+    }
+
+    return { url: prdDBUrl! };
+}
+
+export const initDB = async (): Promise<void> => {
+    try {
+        const { url } = getDBUrl();
+        await connect(url!);
+        console.log('Database connected')
+    } catch (error: any) {
+        throw new DatabaseError("Database initialization failed", { error });
+    }
+}
