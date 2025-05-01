14665f4cef22d4683415541597a3098be6e0da01
Rifushigi <tope.dansa@gmail.com>
2025-05-01 11:00:03 +0100

    Remove device type definitions from the project

--- backend/src/types/device.ts
+++
@@ -1,9 +0,0 @@
-import { ObjectId } from "mongoose";
-
-export interface TDevice {
-    deviceType: string;
-    serialNumber: string;
-    locationDesc: string;
-    lastActive: Date;
-    assignedClass: ObjectId;
-}
