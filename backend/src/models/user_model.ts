bb4070e400532c4ac26f616eb3bbb704d2c34e67
Rifushigi <tope.dansa@gmail.com>
2025-05-03 08:10:25 +0100

    Remove required validation from firstName and lastName fields in user model

--- backend/src/models/user_model.ts
+++ backend/src/models/user_model.ts
@@ -4,8 +4,8 @@ import { model, Schema } from "mongoose";
 const userSchema = new Schema<TUser>({
     avatar: { type: String },
     email: { type: String, required: true, unique: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+    firstName: { type: String },
+    lastName: { type: String },
     password: { type: String },
     role: { type: String, required: true, enum: ['admin', 'lecturer', 'student'] },
     isVerified: { type: Boolean, default: false },
