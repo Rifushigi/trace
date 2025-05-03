125ae432faf2c544a5b2cc5fa89fdf5b02fdce35
Rifushigi <tope.dansa@gmail.com>
2025-05-03 08:10:09 +0100

    Add unique constraint to matricNo in student model

--- backend/src/models/student_model.ts
+++ backend/src/models/student_model.ts
@@ -3,7 +3,7 @@ import { TStudent } from "../types/index.js";
 
 const studentSchema = new Schema<TStudent>({
     userId: { type: mongoose.Types.ObjectId, required: true },
-    matricNo: { type: String, required: true },
+    matricNo: { type: String, required: true, unique: true },
     program: { type: String, required: true },
     level: { type: String, enum: ["100", "200", "300", "400", "500", "600", "700"], required: true },
     faceModelId: { type: String },
