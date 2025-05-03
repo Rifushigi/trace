c576d9274c4e2241d9c374178d81c91056d5715c
Rifushigi <tope.dansa@gmail.com>
2025-05-03 08:09:54 +0100

    Add unique constraint to staffId in lecturer model

--- backend/src/models/lecturer_model.ts
+++ backend/src/models/lecturer_model.ts
@@ -3,7 +3,7 @@ import { TLecturer } from "../types/index.js";
 
 const lecturerSchema = new Schema<TLecturer>({
     userId: { type: mongoose.Types.ObjectId, required: true },
-    staffId: { type: String, required: true },
+    staffId: { type: String, required: true, unique: true },
     department: { type: String, required: true }
 });
 
