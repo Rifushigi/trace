1af430cd353d7e6be9301ba3607bc6d4666a83a3
Rifushigi <rifushigi.teda@gmail.com>
2025-05-05 05:54:22 +0100

    Refactor CORS configuration: update origin and allowedHeaders for improved security and consistency

--- backend/src/config/cors_config.ts
+++ backend/src/config/cors_config.ts
@@ -1,8 +1,11 @@
 import { CorsOptions } from "cors";
 
 export const corsConfig: CorsOptions = {
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3001", "http://localhost:8081"],
-    allowedHeaders: ["*"],
-    methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"],
+    origin: process.env.ENV === 'production'
+        ? ['https://your-production-domain.com']
+        : ['http://localhost:3000', 'http://localhost:5173'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
+    exposedHeaders: ['set-cookie']
 }
--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -3,5 +3,4 @@ export * from './db_config.js';
 export { default as cloudinary } from './cloudinary_config.js';
 export { default as upload } from './multer_config.js'
 export * from './cors_config.js';
-export * from './session_config.js';
 export * from './mailer_config.js';
