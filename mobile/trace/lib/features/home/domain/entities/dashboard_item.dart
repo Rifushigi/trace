2d5cba588eed551d877646ea4430a0a313bbc561
Rifushigi <rifushigi.teda@gmail.com>
2025-05-06 15:34:13 +0100

    Add DashboardItem entity for managing dashboard item properties

---
+++ mobile/trace/lib/features/home/domain/entities/dashboard_item.dart
@@ -0,0 +1,13 @@
+class DashboardItem {
+  final String title;
+  final String route;
+  final String icon;
+  final String description;
+
+  const DashboardItem({
+    required this.title,
+    required this.route,
+    required this.icon,
+    required this.description,
+  });
+} 
