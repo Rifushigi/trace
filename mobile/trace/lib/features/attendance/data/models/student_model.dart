6d8348375868e645bc047b690ca9a84da7367661
Rifushigi <rifushigi.teda@gmail.com>
2025-05-07 22:39:13 +0100

    feat: Add Student model to represent attendance data

---
+++ mobile/trace/lib/features/attendance/data/models/student_model.dart
@@ -0,0 +1,11 @@
+class Student {
+  final String id;
+  final String name;
+  final bool isPresent;
+
+  Student({
+    required this.id,
+    required this.name,
+    required this.isPresent,
+  });
+}
