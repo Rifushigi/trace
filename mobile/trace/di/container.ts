a540aa3e4bf41cd300a134779ca1897a84a725c5
Rifushigi <rifushigi.teda@gmail.com>
2025-05-26 18:34:40 +0100

    feat: implement dependency injection for use cases and repositories in Container and RootStore; add ClassStore and AttendanceStore for enhanced state management

---
+++ mobile/trace/di/container.ts
@@ -0,0 +1,94 @@
+import { AuthUseCase } from '../domain/usecases/auth/AuthUseCase';
+import { AuthUseCaseImpl } from '../domain/usecases/auth/impl/AuthUseCaseImpl';
+import { AuthRepository } from '../domain/repositories/AuthRepository';
+import { AuthRepositoryImpl } from '../data/repositories/AuthRepositoryImpl';
+import { AuthApi } from '../data/datasources/remote/AuthApi';
+import { ProfileUseCase } from '../domain/usecases/profile/ProfileUseCase';
+import { ProfileUseCaseImpl } from '../domain/usecases/profile/impl/ProfileUseCaseImpl';
+import { ProfileRepository } from '../domain/repositories/ProfileRepository';
+import { ProfileRepositoryImpl } from '../data/repositories/ProfileRepositoryImpl';
+import { ProfileApi } from '../data/datasources/remote/ProfileApi';
+import { SettingsUseCase } from '../domain/usecases/settings/SettingsUseCase';
+import { SettingsUseCaseImpl } from '../domain/usecases/settings/impl/SettingsUseCaseImpl';
+import { SettingsRepository } from '../domain/repositories/SettingsRepository';
+import { SettingsRepositoryImpl } from '../data/repositories/SettingsRepositoryImpl';
+import { SettingsApi } from '../data/datasources/remote/SettingsApi';
+import { ClassApi } from '../data/datasources/remote/ClassApi';
+import { AttendanceApi } from '../data/datasources/remote/AttendanceApi';
+import { AttendanceRepositoryImpl } from '../data/repositories/AttendanceRepositoryImpl';
+import { ClassRepositoryImpl } from '../data/repositories/ClassRepositoryImpl';
+import { AttendanceRepository } from '../domain/repositories/AttendanceRepository';
+import { ClassRepository } from '../domain/repositories/ClassRepository';
+import { ClassUseCaseImpl } from '../domain/usecases/class/impl/ClassUseCaseImpl';
+import { AttendanceUseCaseImpl } from '../domain/usecases/attendance/impl/AttendanceUseCaseImpl';
+import { ClassUseCase } from '../domain/usecases/class/ClassUseCase';
+import { AttendanceUseCase } from '../domain/usecases/attendance/AttendanceUseCase';
+
+export class Container {
+    private static instance: Container;
+    private authApi: AuthApi;
+    private authRepository: AuthRepository;
+    private authUseCase: AuthUseCase;
+    private profileApi: ProfileApi;
+    private profileRepository: ProfileRepository;
+    private profileUseCase: ProfileUseCase;
+    private settingsApi: SettingsApi;
+    private settingsRepository: SettingsRepository;
+    private settingsUseCase: SettingsUseCase;
+    private classApi: ClassApi;
+    private attendanceApi: AttendanceApi;
+    private classRepository: ClassRepository;
+    private attendanceRepository: AttendanceRepository;
+    private classUseCase: ClassUseCase;
+    private attendanceUseCase: AttendanceUseCase;
+
+    private constructor() {
+        // Initialize APIs
+        this.authApi = new AuthApi();
+        this.profileApi = new ProfileApi();
+        this.settingsApi = new SettingsApi();
+        this.classApi = new ClassApi();
+        this.attendanceApi = new AttendanceApi();
+
+        // Initialize Repositories
+        this.authRepository = new AuthRepositoryImpl(this.authApi);
+        this.profileRepository = new ProfileRepositoryImpl(this.profileApi);
+        this.settingsRepository = new SettingsRepositoryImpl(this.settingsApi);
+        this.classRepository = new ClassRepositoryImpl(this.classApi);
+        this.attendanceRepository = new AttendanceRepositoryImpl(this.attendanceApi);
+
+        // Initialize Use Cases
+        this.authUseCase = new AuthUseCaseImpl(this.authRepository);
+        this.profileUseCase = new ProfileUseCaseImpl(this.profileRepository);
+        this.settingsUseCase = new SettingsUseCaseImpl(this.settingsRepository);
+        this.classUseCase = new ClassUseCaseImpl(this.classRepository);
+        this.attendanceUseCase = new AttendanceUseCaseImpl(this.attendanceRepository);
+    }
+
+    public static getInstance(): Container {
+        if (!Container.instance) {
+            Container.instance = new Container();
+        }
+        return Container.instance;
+    }
+
+    public getAuthUseCase(): AuthUseCase {
+        return this.authUseCase;
+    }
+
+    public getProfileUseCase(): ProfileUseCase {
+        return this.profileUseCase;
+    }
+
+    public getSettingsUseCase(): SettingsUseCase {
+        return this.settingsUseCase;
+    }
+
+    public getClassUseCase(): ClassUseCase {
+        return this.classUseCase;
+    }
+
+    public getAttendanceUseCase(): AttendanceUseCase {
+        return this.attendanceUseCase;
+    }
+}
--- mobile/trace/stores/RootStore.ts
+++ mobile/trace/stores/RootStore.ts
@@ -1,25 +1,39 @@
 import { AuthUseCase } from '../domain/usecases/auth/AuthUseCase';
 import { ProfileUseCase } from '../domain/usecases/profile/ProfileUseCase';
 import { SettingsUseCase } from '../domain/usecases/settings/SettingsUseCase';
+import { ClassUseCase } from '../domain/usecases/class/ClassUseCase';
+import { AttendanceUseCase } from '../domain/usecases/attendance/AttendanceUseCase';
 import { AuthStore } from './AuthStore';
 import { SettingsStore } from './SettingsStore';
+import { ClassStore } from './ClassStore';
+import { AttendanceStore } from './AttendanceStore';
 
 export class RootStore {
     public readonly authStore: AuthStore;
     public readonly settingsStore: SettingsStore;
+    public readonly classStore: ClassStore;
+    public readonly attendanceStore: AttendanceStore;
     public readonly authUseCase: AuthUseCase;
     public readonly profileUseCase: ProfileUseCase;
     public readonly settingsUseCase: SettingsUseCase;
+    public readonly classUseCase: ClassUseCase;
+    public readonly attendanceUseCase: AttendanceUseCase;
 
     constructor(
         authUseCase: AuthUseCase,
         profileUseCase: ProfileUseCase,
-        settingsUseCase: SettingsUseCase
+        settingsUseCase: SettingsUseCase,
+        classUseCase: ClassUseCase,
+        attendanceUseCase: AttendanceUseCase
     ) {
         this.authUseCase = authUseCase;
         this.profileUseCase = profileUseCase;
         this.settingsUseCase = settingsUseCase;
+        this.classUseCase = classUseCase;
+        this.attendanceUseCase = attendanceUseCase;
         this.authStore = new AuthStore(authUseCase, profileUseCase);
         this.settingsStore = new SettingsStore(settingsUseCase);
+        this.classStore = new ClassStore(classUseCase);
+        this.attendanceStore = new AttendanceStore(attendanceUseCase);
     }
 } 
