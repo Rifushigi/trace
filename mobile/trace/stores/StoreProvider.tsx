80c29a56013d7c22b6b61fcbe5f227f91afa32a5
Rifushigi <rifushigi.teda@gmail.com>
2025-05-26 18:25:00 +0100

    feat: enhance StoreProvider to include ClassUseCase and AttendanceUseCase with memoization for improved performance

--- mobile/trace/stores/StoreProvider.tsx
+++ mobile/trace/stores/StoreProvider.tsx
@@ -1,15 +1,19 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useMemo } from 'react';
 import { StoreContext } from './index';
 import { RootStore } from './RootStore';
 import { AuthUseCase } from '../domain/usecases/auth/AuthUseCase';
 import { ProfileUseCase } from '../domain/usecases/profile/ProfileUseCase';
 import { SettingsUseCase } from '../domain/usecases/settings/SettingsUseCase';
+import { ClassUseCase } from '../domain/usecases/class/ClassUseCase';
+import { AttendanceUseCase } from '../domain/usecases/attendance/AttendanceUseCase';
 
 interface StoreProviderProps {
     children: ReactNode;
     authUseCase: AuthUseCase;
     profileUseCase: ProfileUseCase;
     settingsUseCase: SettingsUseCase;
+    classUseCase: ClassUseCase;
+    attendanceUseCase: AttendanceUseCase;
 }
 
 export const StoreProvider: React.FC<StoreProviderProps> = ({
@@ -17,8 +21,14 @@ export const StoreProvider: React.FC<StoreProviderProps> = ({
     authUseCase,
     profileUseCase,
     settingsUseCase,
+    classUseCase,
+    attendanceUseCase,
 }) => {
-    const store = new RootStore(authUseCase, profileUseCase, settingsUseCase);
+    const store = useMemo(
+        () => new RootStore(authUseCase, profileUseCase, settingsUseCase, classUseCase, attendanceUseCase),
+        [authUseCase, profileUseCase, settingsUseCase, classUseCase, attendanceUseCase]
+    );
+
     return (
         <StoreContext.Provider value={store}>
             {children}
