fa9c5ab107878bf131fc9dbfca940600915cabff
Rifushigi <rifushigi.teda@gmail.com>
2025-05-26 18:23:18 +0100

    refactor: simplify App component by removing unnecessary providers and using Slot for navigation

--- mobile/trace/App.tsx
+++ mobile/trace/App.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { RootNavigator } from './src/navigation/RootNavigator';
-import { StoreProvider } from './src/stores';
+import { Slot } from 'expo-router';
 
 export default function App() {
-    return (
-        <StoreProvider>
-            <SafeAreaProvider>
-                <RootNavigator />
-            </SafeAreaProvider>
-        </StoreProvider>
-    );
+    return <Slot />;
 } 
