64c892d404aa0636250630ed0809349243f565af
Rifushigi <rifushigi.teda@gmail.com>
2025-05-27 14:36:16 +0100

    refactor: replace SystemSettingsScreen implementation with import from presentation layer for better structure

--- mobile/trace/app/(admin)/system-settings.tsx
+++ mobile/trace/app/(admin)/system-settings.tsx
@@ -1,14 +1,2 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { observer } from 'mobx-react-lite';
-import { useStores } from '../../stores';
-
-export default observer(function SystemSettingsScreen() {
-    const { settingsStore } = useStores();
-
-    return (
-        <View>
-            <Text>System Settings</Text>
-        </View>
-    );
-}); 
+import { SystemSettingsScreen } from '../../presentation/screens/admin/SystemSettingsScreen';
+export default SystemSettingsScreen; 
