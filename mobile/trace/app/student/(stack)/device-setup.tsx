cab292015ae17f60e4fbd917ad1fe6dcafad5a08
Rifushigi <rifushigi.teda@gmail.com>
2025-05-30 10:12:11 +0100

    refactor: replace inline DeviceSetupScreen implementation with import from presentation layer for improved modularity

--- mobile/trace/app/student/(stack)/device-setup.tsx
+++ mobile/trace/app/student/(stack)/device-setup.tsx
@@ -1,23 +1,3 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { observer } from 'mobx-react-lite';
-import { useStores } from '../../../stores';
-import { Student } from '../../../domain/entities/User';
+import { DeviceSetupScreen } from "../../../presentation/screens/student/DeviceSetupScreen";
 
-export default observer(function DeviceSetupScreen() {
-    const { authStore } = useStores();
-    const { user } = authStore.state;
-    const student = user as Student;
-
-    return (
-        <View>
-            <Text>Device Setup</Text>
-            {student && (
-                <View>
-                    <Text>Face ID Status: {student.faceModelId ? 'Registered' : 'Not Registered'}</Text>
-                    <Text>NFC Status: {student.nfcUid ? 'Registered' : 'Not Registered'}</Text>
-                </View>
-            )}
-        </View>
-    );
-}); 
+export default DeviceSetupScreen;
